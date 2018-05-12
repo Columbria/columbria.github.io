cb643d0b9e58eb810ed99968929ea428009d0c2a
Columbria <alvis.kundzins@gmail.com>
2018-05-13 01:42:14 +0300

    removed "fixed" property for maps
    Fixed helps to not to stretch images if resiable block drawn out of bondary. But same time it gives glytch with left sidebar.

--- js/map.js
+++ js/map.js
@@ -7,13 +7,13 @@
                range:true,
                min: 1,
                max: 9,
-               values: [ 1, 9 ],
+               values: [ 1, 3 ],
 
                slide: function( event, ui ) {
                   $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
 
-                  $("#resizable").css("background", "url(./img/" + ui.values[ 0 ] + ".jpg)  no-repeat center center fixed");
-                  $("#map-wrapper").css("background", "url(./img/" + ui.values[ 1 ] + ".jpg)  no-repeat center center fixed");
+                  $("#resizable").css("background", "url(./img/" + ui.values[ 0 ] + ".jpg)  no-repeat center center");
+                  $("#map-wrapper").css("background", "url(./img/" + ui.values[ 1 ] + ".jpg)  no-repeat center center");
                   //$("p").css("background-color", "yellow");
                }
             });
